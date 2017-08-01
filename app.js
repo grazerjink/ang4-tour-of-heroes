1caa6908c775e0d6287af9a0cfc4753b8638751f
Khải Quân <khaiquan9926@gmail.com>
2017-08-01 22:13:34 +0800

    The demo of angular 4.x

--- app.js
+++ app.js
@@ -4,7 +4,4 @@ var port = process.env.port || 3000;
 
 app.use("/node_modules", express.static(__dirname + "/node_modules"));
 app.use("/", express.static(__dirname + "/src"));
-app.get("/",(req,res)=> {
-    res.redirect("/index.html");
-})
-app.listen(port, (res)=> console.log("Server is listening on",port));
+app.listen(port);
